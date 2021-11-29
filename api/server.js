ae669a0ff85a1bb09d9d0e4b1099e42f701ba11e
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-11-29 18:13:19 -0500

    add post new user

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const User = require('./users/model')
 const server = express()
 
 // GLOBAL MIDDLEWARE
-//server.use(express.json())
+server.use(express.json())
 
 
 
@@ -19,7 +19,19 @@ server.get('/hello', (req, res)=>{
 
 //| GET    | /api/users     | Returns an array users.     
 
-server.get('/api/users', async (req, res)=>{
+// server.get('/api/users', (req, res)=>{
+//     User.find()
+//     .then(users=>{
+//         res.json(users)
+//     })
+//     .catch(err=>{
+//         res.status(500).json({
+//             message:"The users information could not be retrieved",
+//             error: err.message})
+//         })
+//     })
+
+server.get('/api/users', async (req, res)=>{  
     try{
         const users = await User.find()
         res.json(users)
@@ -31,8 +43,27 @@ server.get('/api/users', async (req, res)=>{
         })
     }
 })
+ 
 
-// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
+// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.   
+
+server.post('/api/users', async (req, res )=>{
+    try{
+        if(!req.body.name || !req.body.bio){
+            res.status(400).json({
+                message:"provide name and bio"
+            })
+        }else{
+            const newUser = await User.insert(req.body)
+            res.status(201).json(newUser)
+        }
+    }catch(err){
+        res.status(400).json({
+            message:"Please provide name and bio for the user",
+            error: err.message
+        }) 
+    } 
+})
 // | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
 // | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
 // | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
